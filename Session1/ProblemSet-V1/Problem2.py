cc535439dda276db8ccef8cbcda2ee237fb69a44
Harshith241 <itzharshith.v06@gmail.com>
2025-06-23 18:00:49 +0530

    problem2 done v1

---
+++ Session1/ProblemSet-V1/Problem2.py
@@ -0,0 +1,20 @@
+'''Write a function swap_ends() that accepts a string my_str as a parameter and returns a new string where the first and last characters from my_str are swapped.
+
+def swap_ends(my_str):
+    pass
+Example Usage:
+
+my_str = "boat"
+swapped = swap_ends(my_str)
+print(swapped)
+Example Output: toab'''
+
+def swap_ends(my_str):
+    first_letter = my_str[0]
+    middle_part = my_str[1:len(my_str) - 1]
+    last_letter = my_str[len(my_str) - 1]
+    return last_letter + middle_part + first_letter
+
+my_str = "boat"
+swapped = swap_ends(my_str)
+print(swapped)
